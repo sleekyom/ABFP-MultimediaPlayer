b35681e74ab8a60d08525475048e4aaca7772a48
Emmanuel Kolawole <amdas72@gmail.com>
2020-07-13 21:30:24 +0300

    Added base components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className="display">
       This is my app
     </div>
   )
