4327cd30fc259c3ed3d7824d6a97e88e6e8c87d3
Emmanuel Kolawole <amdas72@gmail.com>
2020-07-28 21:22:37 +0300

    Updates

---
+++ precache-manifest.03b8607b275f4849d3ab0a17c0d6a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777a4f4b48c8991d3035956e1144cd97",
+    "url": "/ABFP-MultimediaPlayer/index.html"
+  },
+  {
+    "revision": "455ded175ce0789216da",
+    "url": "/ABFP-MultimediaPlayer/static/css/main.9e8f68cb.chunk.css"
+  },
+  {
+    "revision": "6422bfae936275abef4e",
+    "url": "/ABFP-MultimediaPlayer/static/js/2.9d4cbe77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ABFP-MultimediaPlayer/static/js/2.9d4cbe77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455ded175ce0789216da",
+    "url": "/ABFP-MultimediaPlayer/static/js/main.8b0b73bd.chunk.js"
+  },
+  {
+    "revision": "714eae3573b4cb7602d8",
+    "url": "/ABFP-MultimediaPlayer/static/js/runtime-main.2efefdb6.js"
+  }
+]);
