39c9d7f8b5950fa4fb749a10d30f37889cc15948
Emmanuel Kolawole <amdas72@gmail.com>
2020-08-05 21:04:18 +0300

    Bootstrap Components Added

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,27 +2,6 @@ import React, { Component } from 'react';
 import './InputField.css';
 
  class InputField extends Component {
-    // state = {
-    //  urls: [],
-    //  userInput:""
-    // }
-
-    //  trackUrl = (e) => {
-    //     this.setState({userInput: e.target.value});
-    //     this.props.userUrlValue(this.userInput);
-    // }
-
-    //  pushToArray(e){
-    //     e.preventDefault();
-    //     if(this.userInput===""){
-    //         return;
-    //     } else{
-    //         this.urls.push(this.userInput)
-    //         localStorage.userPlaylist = JSON.stringify(this.urls);
-    //         this.userInput="";
-    //         console.log(this.urls);
-    //     }  
-    // }
     
     render(){
         const { trackUrl, userInput, pushToArray, trackName} = this.props
